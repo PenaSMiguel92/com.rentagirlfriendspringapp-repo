c34066b9624fcfe5c0140fc47f13f562aaaafdae
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-27 22:35:23 -0700

    init

---
+++ src/main/java/com/rentagirlfriend/RentAGirlfriendApplication.java
@@ -0,0 +1,13 @@
+package com.rentagirlfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentAGirlfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentAGirlfriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentagirlfriend/RentAGirlfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentagirlfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentAGirlfriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
